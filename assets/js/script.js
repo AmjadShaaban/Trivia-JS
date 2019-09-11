545cc59af1ba88b835142f644c372243c4e972e5
Amjad Shaaban <theamjad@outlook.com>
2019-09-11 13:18:48 -0400

    More Scripting done basic Game times working Click event choices (console log only)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,237 @@
 $(document).ready(() => {
   const questions = [
     {
-      Q: "What is going on!?",
-      A1: "I Don't Know!",
-      A2: "I Do Know!   ",
-      A3: "Maybe I do?",
-      A4: "Fuck! I need HALP!"
+      Q: "Q10 What is going on!?",
+      A1: {
+        Id: "420470b743cc5ded",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "2baf94cae39f2605",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "43b0321c3afb3c1c",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "5f3823799ad4d2a2",
+        Value: "????AM????"
+      }
     },
     {
-      Q: "Q2 Yo! Which shit you pickin?",
-      A1: "Pick dis",
-      A2: "Pick the First on!",
-      A3: "No maybe Dis?",
-      A4: "100% Dis"
+      Q: "Q9 Yo! Which shit you pickin?",
+      A1: {
+        Id: "85cbfc06135bfb0b",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "955bcbfb95f9b418",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "8434faa9ccc1e11b",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "7328a063dac08627",
+        Value: "????AM????"
+      }
     },
     {
-      Q: "Q3 Pick yo shit!?",
-      A1: "DONT! Pick dis",
-      A2: "Dont pick the 1st or 2nd!",
-      A3: "maybe Pick Dis?",
-      A4: "100% Dis"
+      Q: "Q8 Pick yo shit!?",
+      A1: {
+        Id: "f71854db14e73545",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "4b3e865c6a283ad5",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "1f1a4b359681d4c0",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "df55aa22bd7ea7f2",
+        Value: "????AM????"
+      }
+    },
+    {
+      Q: "Q7 ??????????????Hi??????????????",
+      A1: {
+        Id: "7af448886f225658",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "a56a0d8e1a3e8b36",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "4a5d1efc659bc87c",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "c14763af0c5da0df",
+        Value: "????AM????"
+      }
+    },
+    {
+      Q: "Q6 ??????????????Hi??????????????",
+      A1: {
+        Id: "0c2466c6b657d0ee",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "158f69577e3e0fa2",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "098b031bd43aedbd",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "d657c9d60f569188",
+        Value: "????AM????"
+      }
+    },
+    {
+      Q: "Q5 ??????????????Hi??????????????",
+      A1: {
+        Id: "2832e8ec9483ac56",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "5a5ac84883213600",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "29d26ddc060329db",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "3a4b00decfaa6a18",
+        Value: "????AM????"
+      }
     },
     {
       Q: "Q4 ??????????????Hi??????????????",
-      A1: "????PM????",
-      A2: "????NO????",
-      A3: "????Hi????",
-      A4: "????AM????"
+      A1: {
+        Id: "8fc8a862dd920e5e",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "db96363c44aa4af6",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "32f758010303f43c",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "2b48073cad8d8829",
+        Value: "????AM????"
+      }
+    },
+    {
+      Q: "Q3 ??????????????Hi??????????????",
+      A1: {
+        Id: "d0b4d587a41926f1",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "d3dbcc3dd45bcd0f",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "de9a48cb99e9a936",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "cb5a834f0380b2b8",
+        Value: "????AM????"
+      }
+    },
+    {
+      Q: "Q2 ??????????????Hi??????????????",
+      A1: {
+        Id: "b5909ad8c8cee52b",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "96bd00f0c2f9d673",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "bcbc90ca0d3ba708",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "1e7713aace897d9f",
+        Value: "????AM????"
+      }
+    },
+    {
+      Q: "Q1 ??????????????Hi??????????????",
+      A1: {
+        Id: "67d259092c325b13",
+        Value: "????PM????"
+      },
+      A2: {
+        Id: "9c2db64b1573a8cb",
+        Value: "????NO????"
+      },
+      A3: {
+        Id: "02366a0f0420b587",
+        Value: "????Hi????"
+      },
+      A4: {
+        Id: "60dd3b848a01b827",
+        Value: "????AM????"
+      }
     }
   ];
 
-  const questionsMap = {
-    Q: "Question",
-    A1: "First Choice",
-    A2: "Second Choice",
-    A3: "Third Choice",
-    A4: "Forth Choice"
-  };
   const TIME_PER_QUESTION = 30;
+  var right = 0;
+  var wrong = 0;
   //   var i = 0;
   var numberOfQuestions = questions.length;
   var theLooper = numberOfQuestions => {
     setTimeout(() => {
       $(".question-card").remove();
       numberOfQuestions--;
-      if (numberOfQuestions > 0) {
-        console.log(numberOfQuestions - 1);
+      if (numberOfQuestions >= 0) {
+        console.log(numberOfQuestions);
         console.log("going again");
-        createQuestionCard(numberOfQuestions - 1);
+        createQuestionCard(numberOfQuestions);
+        $(".choices").on("click", function() {
+          console.log("clicked!");
+          console.log(parseInt($(this).attr("id"), 16));
+          if (
+            parseInt($(this).attr("id"), 16) === triviaKey[numberOfQuestions]
+          ) {
+            console.log("CORRECT!");
+          } else {
+            console.log("WRONG!!!");
+          }
+        });
         theLooper(numberOfQuestions);
       }
-    }, 5000);
+    }, TIME_PER_QUESTION * 1000);
   };
 
-  var qTimer = () => {
+  var startTrivia = () => {
     console.log("Starting!");
-    createQuestionCard(numberOfQuestions - 1);
+    var startCard = $("<div>"),
+      heading = $("<h1>"),
+      startBtn = $("<button>");
+    startCard.attr("class", "card question-card");
+    heading.text("Welcome to TriFia");
+    startBtn.text("START!");
+    heading.appendTo(startCard);
+    startBtn.appendTo(startCard);
+    startCard.appendTo($(".trivia-questions"));
     theLooper(numberOfQuestions);
   };
 
@@ -81,14 +257,18 @@ $(document).ready(() => {
     t.attr("class", "card-header timer");
     q.attr("class", "question");
     cBox.attr("class", "list-group list-group-flush");
-    c1.attr("class", "list-group-item");
-    c2.attr("class", "list-group-item");
-    c3.attr("class", "list-group-item");
-    c4.attr("class", "list-group-item");
-    c4.text(questions[i].A4);
-    c3.text(questions[i].A3);
-    c2.text(questions[i].A2);
-    c1.text(questions[i].A1);
+    c1.attr("class", "list-group-item choices");
+    c1.attr("id", questions[i].A1.Id);
+    c2.attr("class", "list-group-item choices");
+    c2.attr("id", questions[i].A2.Id);
+    c3.attr("class", "list-group-item choices");
+    c3.attr("id", questions[i].A3.Id);
+    c4.attr("class", "list-group-item choices");
+    c4.attr("id", questions[i].A4.Id);
+    c4.text(questions[i].A4.Value);
+    c3.text(questions[i].A3.Value);
+    c2.text(questions[i].A2.Value);
+    c1.text(questions[i].A1.Value);
     q.text(questions[i].Q);
     c1.appendTo(cBox);
     c2.appendTo(cBox);
@@ -99,5 +279,17 @@ $(document).ready(() => {
     t.appendTo(qCard);
     qCard.appendTo($(".trivia-questions"));
   };
-  qTimer();
+  var triviaKey = [
+    4877453493272263708,
+    9641076530341477131,
+    16092955909663139826,
+    8859786117761881688,
+    874937231265485038,
+    3013591892425386459,
+    15822893973290765046,
+    14653168712987751096,
+    2195244967696891295,
+    159431449544537479
+  ];
+  startTrivia();
 });
